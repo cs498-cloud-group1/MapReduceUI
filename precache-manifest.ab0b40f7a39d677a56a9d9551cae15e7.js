46f21a1e0ae086ccec4d9d14f573dade56e2be15
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-30 20:26:04 +0000

    Deploy cs498-cloud-group1/MapReduceUI to github.com/cs498-cloud-group1/MapReduceUI.git:gh-pages

--- precache-manifest.ab0b40f7a39d677a56a9d9551cae15e7.js
+++ precache-manifest.ab0b40f7a39d677a56a9d9551cae15e7.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "https://cs498-cloud-group1.github.io/MapReduceUI/static/js/runtime~main.5e36ca60.js"
   },
   {
-    "revision": "d5995b5d7712f84f965c",
-    "url": "https://cs498-cloud-group1.github.io/MapReduceUI/static/js/main.b4107a0e.chunk.js"
+    "revision": "266385371b513434521f",
+    "url": "https://cs498-cloud-group1.github.io/MapReduceUI/static/js/main.6388a0cb.chunk.js"
   },
   {
     "revision": "567ac5faaefd72650f8d",
     "url": "https://cs498-cloud-group1.github.io/MapReduceUI/static/js/2.0bb7e74e.chunk.js"
   },
   {
-    "revision": "d5995b5d7712f84f965c",
+    "revision": "266385371b513434521f",
     "url": "https://cs498-cloud-group1.github.io/MapReduceUI/static/css/main.8eac81fb.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "https://cs498-cloud-group1.github.io/MapReduceUI/static/css/2.40e01b66.chunk.css"
   },
   {
-    "revision": "71751eb616898d56f34ac3d27e423707",
+    "revision": "440b4e92a52e6c0642800cc0c1de0264",
     "url": "https://cs498-cloud-group1.github.io/MapReduceUI/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://cs498-cloud-group1.github.io/MapReduceUI/precache-manifest.8c6f3ef1c02768e32a780cccc179e28c.js"
+  "https://cs498-cloud-group1.github.io/MapReduceUI/precache-manifest.ab0b40f7a39d677a56a9d9551cae15e7.js"
 );
 
 workbox.clientsClaim();
