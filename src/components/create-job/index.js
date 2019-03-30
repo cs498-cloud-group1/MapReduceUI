8c7f6aabe4b5942284f55e29b17a13365edcf068
Joel Detweiler <joel.detweiler@gmail.com>
2019-03-30 14:47:06 -0400

    Create page for making a job

---
+++ src/components/create-job/index.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import isEmpty from 'lodash/isEmpty';
+
+import * as api from '../../api';
+
+import './index.scss';
+
+export default class CreateJob extends Component {
+  state = {
+    form: {
+      jobName: '',
+      url: '',
+      map: '',
+      reduce: ''
+    },
+    loading: false
+  };
+
+  handleChange = field => event => {
+    this.setState({
+      form: { ...this.state.form, [field]: event.target.value }
+    });
+  };
+
+  submit = () => {
+    if (Object.values(this.state.form).some(isEmpty)) {
+      alert('You must fill out all fields');
+      return;
+    }
+    this.setState({ loading: true });
+    api
+      .createJob(this.state)
+      .then(response => {
+        console.log(response);
+        // go to result page now
+      })
+      .catch(error => {
+        alert(
+          'There was an error submitting your job. Error message: \n' + error
+        );
+      })
+      .finally(() => this.setState({ loading: false }));
+  };
+
+  render() {
+    if (this.state.loading) {
+      return <div className="loader" />;
+    }
+    return (
+      <div className="create-job">
+        <table>
+          <thead>
+            <tr>
+              <th>Field</th>
+              <th>Input</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Job Name</td>
+              <td>
+                <input
+                  type="text"
+                  value={this.state.form.jobName}
+                  onChange={this.handleChange('jobName')}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Data URL</td>
+              <td>
+                <input
+                  type="url"
+                  value={this.state.form.url}
+                  onChange={this.handleChange('url')}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Map Function</td>
+              <td>
+                function map(key, value) {'{'}
+                <textarea
+                  rows="10"
+                  value={this.state.form.map}
+                  onChange={this.handleChange('map')}
+                />
+                {'}'}
+              </td>
+            </tr>
+            <tr>
+              <td>Reduce</td>
+              <td>
+                function reduce(key, values) {'{'}
+                <textarea
+                  rows="10"
+                  value={this.state.form.reduce}
+                  onChange={this.handleChange('reduce')}
+                />
+                {'}'}
+              </td>
+            </tr>
+            <tr>
+              <td />
+              <td>
+                <button className="submit" onClick={this.submit}>
+                  Submit
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
