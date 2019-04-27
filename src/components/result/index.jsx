0dc7921292b30b98bd41f5808f2308253008abc5
Arif Topcu <atopcu2@illinois.edu>
2019-04-27 15:02:02 -0400

    result page work

--- src/components/result/index.jsx
+++ src/components/result/index.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import AceEditor from 'react-ace';
 
 import * as api from '../../api';
 
 import '../common.scss';
 import './index.scss';
+
+const aceOptions = {
+  theme: 'chrome',
+  mode: 'javascript',
+  height: '150px',
+  highlightActiveLine: true,
+  setOptions: {
+    showLineNumbers: true,
+    tabSize: 2
+  },
+  editorProps: { $blockScrolling: true }
+};
 
 export default class ListResults extends Component {
   state = {
@@ -19,13 +32,64 @@ export default class ListResults extends Component {
   }
 
   render() {
-    const job = this.state.job;
+    const job = this.state.job || {};
     if (this.state.loading) {
       return <div className="loader" />;
     }
+    const {
+      createdAt,
+      updatedAt,
+      status,
+      jobId,
+      map,
+      reduce,
+      url,
+      jobName
+    } = job;
     return (
-      <div className="result">
-        <pre>{JSON.stringify(job, null, 2)}</pre>
+      <div className="result__root">
+        <div className="body">
+          <div className="header">
+            <div>
+              <div className="jobName">{jobName}</div>
+              <div className="subHeader">
+                <div className="label">Created On:</div>
+                <div className="value">
+                  {new Date(createdAt).toDateString()}
+                </div>
+                <div className="label">Last Update:</div>
+                <div className="value">
+                  {new Date(updatedAt).toDateString()}
+                </div>
+              </div>
+              <div>
+                Input: <a href={url}>{url}</a>
+              </div>
+            </div>
+            <div className="status">
+              <div>Status:</div>
+              <div className="value">{status ? status.toUpperCase() : ''}</div>
+            </div>
+          </div>
+          <div className="function">
+            <div className="function_name">Map:</div>
+            <AceEditor
+              name="map-function"
+              readOnly
+              value={map}
+              {...aceOptions}
+            />
+          </div>
+          <div className="function">
+            <div className="function_name">Reduce:</div>
+            <AceEditor
+              name="reduce-function"
+              readOnly
+              value={reduce}
+              {...aceOptions}
+            />
+          </div>
+        </div>
       </div>
     );
   }
